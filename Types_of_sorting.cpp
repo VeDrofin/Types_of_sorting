a83df2cdf093e8db26512d990518dce5ca810f87
Дмитрий <xmasbass666@gmail.com>
2022-12-27 00:33:36 +0300

    Change to dynamic massiv

--- Types_of_sorting.cpp
+++ Types_of_sorting.cpp
@@ -5,30 +5,24 @@
 #include <time.h>
 #include <fstream>
 #include <chrono>
+#include <string>
 
 
 // creates a vector of random numbers with length n and less than range
-int * make_dar(int n, int range)
+int * make_array(int n, int range)
 {
-    int *  list= new int[n];
+    int * list = new int[n];
     for (int i = 0; i < n; i++)
         list[i] = (rand() * rand()) % range;
     return(list);
 }
-std::vector<int> make_array(int n, int range)
-{
-    std::vector<int> list(n);
-    for (int i = 0; i < n; i++)
-        list[i] = (rand() * rand()) % range;
-    return(list);
-}
-void swap(std::vector<int> arr, int i, int I) {
+void swap(int * arr, int i, int I) {
     int temp = arr[i];
     arr[i] = arr[I];
     arr[I] = temp;
 }
 
-void HeapSort1(std::vector<int> arr, int size, int i) {
+void HeapSort1(int * arr, int size, int i) {
     int big = i;
     for (int j = i * 2 + 1; j < i * 2 + 3 && j < size; j++) {
         if (arr[big] < arr[j]) {
@@ -40,7 +34,7 @@ void HeapSort1(std::vector<int> arr, int size, int i) {
         HeapSort1(arr, size, big);
     }
 }
-void HeapSort2(std::vector<int> arr, int size) {
+void HeapSort2(int * arr, int size) {
     for (int i = size / 2 - 1; i >= 0; i--) {
         HeapSort1(arr, size, i);
     }
@@ -51,7 +45,7 @@ void HeapSort2(std::vector<int> arr, int size) {
     }
 }
 
-void ShellSort(int n, std::vector<int> mass)
+void ShellSort(int n, int * mass)
 {
 
     int i, j, step;
@@ -73,7 +67,7 @@ void ShellSort(int n, std::vector<int> mass)
     //    std::cout << i << ' ';
 }
 
-void InsertionSort(int n, std::vector<int> mass)
+void InsertionSort(int n, int * mass)
 {
 
     for (int i = 1; i < n; i++)
@@ -101,30 +95,75 @@ void InsertionSort(int n, std::vector<int> mass)
 int main()
 {
     srand(time(NULL));
+
+    int size2 = 100000; // For IsertionSort
     int size = 10000000; // enter the size of the array
-    std::vector<int> mass = make_array(size, 100000000);
-
     std::ofstream out("sorting_time.txt", std::ios::app);
-    std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
-    HeapSort2(mass, size);
-    std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
-    std::chrono::duration<double> sec = end - start;
-    out << "HeapSort "
-        << int(sec.count()) << " sec \n";
+    for (int p = 0; p < 10; p++) {
+        std::cout << "cont " << p << std::endl;
+        int * mass = make_array(size,  size);
+        int * mass2 = make_array(size2, size2);
+        std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+        HeapSort2(mass2, size2);
+        std::cout << "step 1" << std::endl;
+        std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+        std::chrono::duration<double> sec = end - start;
+        out << "HeapSort" << std::endl
+            << int(sec.count()) << std::endl;
 
-    std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
-    ShellSort(size, mass);
-    std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
-    std::chrono::duration<double> sec = end - start;
-    out << "Shell Sort: "
-        << int(sec.count()) << " sec \n";
+        start = std::chrono::system_clock::now();
+        ShellSort(size, mass);
+        std::cout << "step 2" << std::endl;
+        end = std::chrono::system_clock::now();
+        sec = end - start;
+        out << "ShellSort" << std::endl
+            << int(sec.count()) << std::endl;
 
-    start = std::chrono::system_clock::now();
-    InsertionSort(size, mass);
-    end = std::chrono::system_clock::now();
-    sec = end - start;
-    out << "Insertion Sort: "
-        << int(sec.count()) << " sec \n";
-
+        start = std::chrono::system_clock::now();
+        InsertionSort(size2, mass2);
+        std::cout << "step 3" << std::endl;
+        end = std::chrono::system_clock::now();
+        sec = end - start;
+        out << "InsertionSort" << std::endl
+            << int(sec.count()) << std::endl;
+    }
+    double time; std::string line;
     out.close();
+    //Подсчет рез-та в файл "Final Res.txt"
+    std::ofstream out2("Final Res.txt");
+    std::ifstream file("sorting_time.txt");
+    if (file.is_open() && out2.is_open()) {
+        double HeapS = 0, Shell = 0, Insert = 0, Summtime = 0; // Суммарные времена
+        int HeapSc = 0, Shellc = 0, Insertc = 0; //Кол - во включений ф-й
+        for (int i = 1; i <= 3 * 10; i++) {
+            getline(file,line);
+            if (line == "HeapSort") {
+                getline(file, line);
+                time = std::stod(line);
+                HeapS += time;
+                HeapSc += 1;
+                Summtime += time;
+            }
+            if (line == "ShellSort") {
+                getline(file, line);
+                time = std::stod(line);
+                Shell += time;
+                Shellc += 1;
+                Summtime += time;
+            }
+            if (line == "InsertionSort") {
+                getline(file, line);
+                time = std::stod(line);
+                Insert += time;
+                Insertc += 1;
+                Summtime += time;
+            }
+        }
+        out2 << "RES_FOR_HEAP_SORT : " << (double)HeapS / HeapSc << "sec \n"
+            << "RES_FOR_SHELL_SORT : " << (double)(Shell / Shellc) << "sec \n"
+            << "RES_FOR_INSERT_SORT : " << (double)(Insert / Insertc) << "sec \n";
+        out2.close();
+        file.close();
+    }
+    return 0;
 }
